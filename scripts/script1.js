741ff7f7ed0c936c6babc966f1177c60d88163c9
G.R. Ali Quaderi <quaderinayeem@gmail.com>
2025-10-02 14:22:45 +0600

    added love function

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,1 +1,14 @@
-console.log("connected")
+let currentCoin=parseInt(document.getElementById('coin-number').innerText)
+let currentHeart=parseInt(document.getElementById('heart-number').innerText)
+let currentCopied=parseInt(document.getElementById('copy-number').innerText)
+const loves=document.getElementsByClassName('loved')
+for(love of loves)
+{
+    love.addEventListener('click', function(e) {
+        currentHeart++;
+        document.getElementById('heart-number').innerText=currentHeart;
+        e.target.classList.add("love-hover");
+        
+    });
+
+}
