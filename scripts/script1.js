bd48d7b3f5840107826c4fc4391e8ca01fb6fb28
G.R. Ali Quaderi <quaderinayeem@gmail.com>
2025-10-02 15:00:03 +0600

    added calling functionality

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,14 +1,33 @@
-let currentCoin=parseInt(document.getElementById('coin-number').innerText)
+let currentCoin=document.getElementById('coin-number').innerText;
 let currentHeart=parseInt(document.getElementById('heart-number').innerText)
 let currentCopied=parseInt(document.getElementById('copy-number').innerText)
 const loves=document.getElementsByClassName('loved')
 for(love of loves)
 {
     love.addEventListener('click', function(e) {
+        if(e.target.dataset.clicked==='true'){
+            alert("Already added to favourite")
+            return;
+        }
+        e.target.dataset.clicked='true';
         currentHeart++;
         document.getElementById('heart-number').innerText=currentHeart;
         e.target.classList.add("love-hover");
-        
+
     });
 
+}
+const dial=document.getElementsByClassName('dial');
+for(call of dial){
+    call.addEventListener("click",function(e){
+        let balance=parseInt(currentCoin);
+        if(balance<20){
+            alert("Not enough coin!Please Recharge");
+        }else{
+            balance=balance-20;
+            currentCoin=(balance);
+            document.getElementById('coin-number').innerText=balance.toString();
+            alert("Calling");
+        }
+    });
 }
